c98dcf0c812c19706753bfd68201515a0cc2129e
Sahm Abu Hashish <archi.sahm@gmail.com>
2023-07-26 10:40:00 +0000

    39

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,29 @@
-export default function Footer({ reports }) {
-    const totalRows = reports;
-    // const totalRows = reports.length;
+// export default function Footer({ reports }) {
+//     const totalRows = reports;
+//     // const totalRows = reports.length;
+//     return (
+//         <footer className="text-black-50 bg-green-500 p-4 mt-4 text-gray-800">
+//             {totalRows} location world wide
+//         </footer>
+//     );
+// }
+
+import { useAuth } from "../contexts/auth";
+
+export default function Header() {
+    const { user, logout } = useAuth();
+
     return (
-        <footer className="text-black-50 bg-green-500 p-4 mt-4 text-gray-800">
-            {totalRows} location world wide
-        </footer>
+      <header className="bg-green-500 text-black-50 p-4 text-gray-800">
+        <h1 className="text-4xl">Cookie Stand Admin</h1>
+        {user ? (
+        <>
+          <p>Welcome, {user.username}!</p>
+          <button className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600"  onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <p>Please log in</p>
+      )}
+      </header>
     );
-}
+  }
--- pages/index.js
+++ pages/index.js
@@ -5,26 +5,42 @@ import Header from '../components/Header';
 import CreateForm from '../components/CreateForm';
 import ReportTable from '../components/ReportTable';
 import Footer from '../components/Footer';
+import CookieStandAdmin from '../components/CookieStandAdmin';
+import LoginForm from '../components/LoginForm';
+import { useAuth } from "../contexts/auth"
 
-export default function CookieStandAdmin() {
+
+
+export default function Home() {
+
+  const { login, user, logout } = useAuth();
   const [cookieReports, setCookieReports] = useState(reports);
 
   return (
     <>
-     <div className="min-h-screen bg-gray-100">
-     <Head>
-       <title>Cookie Stand Admin</title>
-     </Head>
-      <Header />
-      <main className="container mx-auto p-4">
-        <CreateForm onCookieReportCreate={handleCookieReportCreate} />
-        {cookieReports.length === 0 ? (
-          <h2 className="text-center text-2xl mt-8">No Cookie Stands Available</h2>
-        ) : (
-          <ReportTable reports={cookieReports} />
-        )}
-      </main>
-      <Footer reports={cookieReports} />
+      <div className="min-h-screen bg-gray-100">
+        <Head>
+          <title>Cookie Stand Admin</title>
+        </Head>
+        <Header />
+
+        {/* <main className="container mx-auto p-4">
+          <CreateForm onCookieReportCreate={handleCookieReportCreate} />
+          {cookieReports.length === 0 ? (
+            <h2 className="text-center text-2xl mt-8">No Cookie Stands Available</h2>
+          ) : (
+            <ReportTable reports={cookieReports} />
+          )}
+        </main> */}
+        <main className="p-4">
+          {user ? (
+            <CookieStandAdmin />
+          ) : (
+            <LoginForm />
+          )}
+        </main>
+
+        <Footer reports={cookieReports} />
       </div>
     </>
   );
